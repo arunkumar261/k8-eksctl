bf6e530ad8fbc48456a179f30171baa7aa85dc1b
arunkumar261 <arunkumarreddy261@gmail.com>
2024-03-18 23:10:18 +0530

    workstation ec2 for kubernetes along with eksctl installed

--- workstation.sh
+++ workstation.sh
@@ -64,10 +64,12 @@ chmod +x kubectl
 
 mv kubectl /usr/local/bin/kubectl
 
-VALIDATE $? 'kubectl installation"
+VALIDATE $? "kubectl installation"
 
 curl -sLO "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_$PLATFORM.tar.gz"
 
 tar -xzf eksctl_$PLATFORM.tar.gz -C /tmp && rm eksctl_$PLATFORM.tar.gz
 
 sudo mv /tmp/eksctl /usr/local/bin
+
+VALIDATE $? "eksctl installation"
